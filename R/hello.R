e7d22891922cd5e93b6618881457d6affeec63fb
Jason Lai <jasonywlai@gmail.com>
2022-04-09 09:43:55 -0400

    test roxygen header

--- R/hello.R
+++ R/hello.R
@@ -6,6 +6,16 @@ goodbye <- function() {
   print("goodbye!")
 }
 
+#' @title Another One
+#'
+#' @description
+#' Another function to test roxygen automation
+#'
+#' @details
+#' Yet another one
+#'
+#' @export
+#'
 another_one <- function() {
   print("another one")
 }
